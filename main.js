217a6fb33b8ffacf2b09ced5ae526f5c9660b75a
prachiwhitehatjr <123462997+prachiwhitehatjr@users.noreply.github.com>
2023-03-27 12:44:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -48,5 +48,23 @@ else{
 console.log(results);
 document.getElementById("result1").innerHTML=results[0].label;
 document.getElementById("result2").innerHTML=results[1].label;
+if(results[0].label=="happy"){
+    document.getElementById("update_emoji1").innerHTML="&#128522"
+}
+if(results[0].label=="sad"){
+    document.getElementById("update_emoji1").innerHTML="&#128532"
+}
+if(results[0].label=="angry"){
+    document.getElementById("update_emoji1").innerHTML="&#128548"
+}
+if(results[1].label=="happy"){
+    document.getElementById("update_emoji2").innerHTML="&#128522"
+}
+if(results[1].label=="sad"){
+    document.getElementById("update_emoji2").innerHTML="&#128532"
+}
+if(results[1].label=="angry"){
+    document.getElementById("update_emoji2").innerHTML="&#128548"
+}
 }
 }
